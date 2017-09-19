7fa0247759e6b4bb8c04d7b6fe694da3f8f326d9
Manuel Tolentino <manutm24@gmail.com>
2017-09-19 17:19:33 -0400

    Servlet contacto funcionando!

--- src/ContactoServlet.java
+++ src/ContactoServlet.java
@@ -6,12 +6,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ContactoServlet", urlPatterns = "/contacto")
 public class ContactoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        try {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>" + req.getParameter("nombre") + "</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hola mundo!"+ req.getParameter("nombre") + "</h1>");
+            out.print("<img src=\"./logojH.jpg\"></img>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+
+//        super.doGet(req, resp);
 
 
     }
